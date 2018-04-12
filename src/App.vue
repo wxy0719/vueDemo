72eceaad2f8a085e115ea18b509d478f4db4b514
wxy0719 <33924704+wxy0719@users.noreply.github.com>
2018-04-12 17:39:06 +0800

    数据列表

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,39 @@
 <template>
   <div id="app">
+    <Header></Header>
     <img src="./assets/logo.png">
     <router-view/>
+    <DataTable :column_list="column_list" :data_list="data_list"></DataTable>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
+import Header from './components/header.vue'
+import DataTable from './components/dataTable.vue'
+import Footer from './components/footer.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {Header, DataTable, Footer},
+  data() {
+    return {
+      'column_list': ['ID','作者','时间','标题','点击次数'],
+      'data_list': []
+    }
+  },
+  created () {
+    this.getData()
+  },
+  methods: {
+    getData () {
+      this.$index_tableData_api.get('topics', null, r => {
+        this.data_list = r.data
+      })
+    }
+  }
 }
+
 </script>
 
 <style lang="css">
