5bab5823fc8577388aeb46a0917cd8d2f6b97688
wxy0719 <33924704+wxy0719@users.noreply.github.com>
2018-06-13 18:13:49 +0800

    菜单懒加载

--- src/components/basic/tree.vue
+++ src/components/basic/tree.vue
@@ -1,32 +1,8 @@
 <template>
   <ul class="layui-nav layui-nav-tree" lay-filter="demo">
-    <!--
-    只循环展示3级菜单
-    -->
     <li :class="[index == 0 ? 'layui-nav-item layui-nav-itemed' : 'layui-nav-item']" v-for="(d,index) in dataList">
-      <a v-if="d[0]" :href="d[2]">{{d[1]}}</a>
-      <dl class="layui-nav-child" v-if="d[3].length">
-        <dd v-for="d_1 in d[3]">
-          <!-- 直接跳转用a标签，子路由用router-link -->
-          <!-- type: 0，路由跳转；1，自身跳转；2，新窗口跳转；9，新一层父级菜单  -->
-          <router-link v-if="d_1[3] == 0" :to="d_1[2]">{{d_1[1]}}</router-link>
-          <a v-else-if="d_1[3] == 1" href="javascript:;" @click="openWebSite(d_1[2],'_self')" >{{d_1[1]}}</a>
-          <a v-else-if="d_1[3] == 2" href="javascript:;" @click="openWebSite(d_1[2],'_blank')" >{{d_1[1]}}</a>
-
-          <!-- 第三级菜单 -->
-          <li v-else-if="d_1[3] == 9 && d_1[4].length" class="layui-nav-item" >
-            <a v-if="d_1[0]" :href="d_1[2]" style="background-color: #393D49 !important;margin-right:8px;">{{d_1[1]}}</a>
-            <dl class="layui-nav-child" style="width:172px !important;">
-              <dd v-for="d_2 in d_1[4]">
-                <router-link v-if="d_2[3] == 0" :to="d_2[2]">{{d_2[1]}}</router-link>
-                <a v-else-if="d_2[3] == 1" href="javascript:;" @click="openWebSite(d_2[2],'_self')" >{{d_2[1]}}</a>
-                <a v-else-if="d_2[3] == 2" href="javascript:;" @click="openWebSite(d_2[2],'_blank')" >{{d_2[1]}}</a>
-              </dd>
-            </dl>
-          </li>
-
-        </dd>
-      </dl>
+      <a v-if="d[0]" :href="d[2]" @click="openMenu(d[3],d[4])">{{d[1]}}</a>
+      <div :id="d[3]" ></div>
     </li>
   </ul>
 </template>
@@ -46,8 +22,22 @@
       getData() {
 
       },
-      openWebSite(url,type){
-        window.open(url,type);
+      openMenu(menuId,grade){
+        //懒加载菜单信息
+        if(localStorage.esteban_TOKEN!=null&&localStorage.esteban_TOKEN!=""){
+          let params = {
+            parentId:menuId,
+            grade:grade
+          }
+          this.$baseAPI.get('interfaceAdapter', params ,10005 ,localStorage.esteban_TOKEN , r => {
+            r.menuList
+            console.log(r.menuList)
+          }, r => {
+            layer.alert(r.message)
+          })
+        }else{
+          console.log("本地token缺失，无法获取菜单列表")
+        }
       }
     }
   }
